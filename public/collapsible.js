48bad2d9ba19c4a079529d90ca1bc583f104a819
victorialena <victoria.magdalena.dax@gmail.com>
2024-07-27 15:20:12 -0700

    Adjust collapsible scroll height

--- public/collapsible.js
+++ public/collapsible.js
@@ -10,7 +10,7 @@ for (i = 0; i < coll.length; i++) {
     if (content.style.maxHeight){
       content.style.maxHeight = null;
     } else {
-      content.style.maxHeight = content.scrollHeight + "px";
+      content.style.maxHeight = "100%"; // content.scrollHeight + "px";
     }
   });
 }
