d6f13d92506e2e1ce01493edec2aa0a6c42ec59f
victorialena <victoria.magdalena.dax@gmail.com>
2024-07-28 18:24:49 -0700

    Enable scrolling each collapsible

--- public/collapsible.js
+++ public/collapsible.js
@@ -1,16 +1,16 @@
 const DEFAULT_OVERLAY_TEXT = "[you should never see this]";
 
 var coll = document.getElementsByClassName("collapsible");
-var i;
+// var i;
 
-for (i = 0; i < coll.length; i++) {
+for (var i = 0; i < coll.length; i++) {
   coll[i].addEventListener("click", function() {
     this.classList.toggle("active");
     var content = this.nextElementSibling;
     if (content.style.maxHeight){
       content.style.maxHeight = null;
     } else {
-      content.style.maxHeight = "100%"; // content.scrollHeight + "px";
+      content.style.maxHeight =  content.scrollHeight + "50px"; // "100%" 
     }
   });
 }
